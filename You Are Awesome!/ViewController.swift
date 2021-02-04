06953c77a0ba0ce2f847d6c5d8552478796ec8da
Jacob P Wang <59838504+jacobwang7@users.noreply.github.com>
2021-02-03 21:05:24 -0500

    Added if statements

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -18,10 +18,27 @@ class ViewController: UIViewController {
     }
 
     @IBAction func messageButton(_ sender: UIButton) {
-        messageLabel.text = "You Are Awesome!"
-        imageView.image = UIImage(named: "image0")
+        let awesomeMessage = "You Are Awesome!"
+        let greatMessage = "You Are Great!"
+        let bombMessage = "You Are Da Bomb!"
+        
+        if messageLabel.text == awesomeMessage{
+            messageLabel.text = greatMessage
+            imageView.image = UIImage(named: "image1")
+            
+        } else if messageLabel.text == "You Are Great!"{
+            messageLabel.text = bombMessage
+            imageView.image = UIImage(named: "image2")
+            
+        }
+        else {
+            messageLabel.text = awesomeMessage
+            imageView.image = UIImage(named: "image0")
+        }
+            
+        }
     }
    
     
-}
+
 
